67796c1c216e5ca8a23d46e1bd293b801cd44166
raymand211092 <50679989+raymand211092@users.noreply.github.com>
2020-08-25 19:58:58 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 import socket
-import sys
-import time
 #import os
 import telebot
 
@@ -11,24 +9,14 @@ ip = "im.todus.cu"
 port = 22
 #ip = ['google.com', 'yandex.ru', 'facebook.com']
 
-def tcpCheck(ip, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(timeout)
-    try:
-        s.connect((ip, int(port)))
-        s.shutdown(socket.SHUT_RDWR)
-        return True
-    except:
-        return False
-    finally:
-        s.close()
-
-def checkHost(ip,port):
-    ipup = False
-    for i in range(retry):
-         if tcpCheck(host["ip"], host["port"]):
-                ipup = True
-                break
-            else:
-                printD('toDus is down!')
-                bot.send_message(channel, val + ' is down!')
+def checkport(port):
+    socket_objet = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port_is = socket_objet.connect_ex((ip, port))
+    socket_objet.close()
+    if port_is == 0;
+        return ("toDus esta funcionando")
+        bot.send_message(channel, "toDus esta funcionando")
+    else:
+        return ("Hay problemas de conexion al toDus")
+        bot.send_message(channel, "Hay problemas de conexion al toDus")
+    
