0176074c864c82b068d49244ef6b34b3285a0cb4
raymand211092 <50679989+raymand211092@users.noreply.github.com>
2020-08-25 19:16:11 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,42 @@
-import os
+import socket
+import sys
+import time
+#import os
 import telebot
 
 channel = '-1001420899246'
 token = '903986387:AAEOTXBHwGkilq9PCvC9NkfohZH259H7pC0'
 bot = telebot.TeleBot(token)
-ip = ["im.todus.cu", "www.todus.cu", "s3.todus.cu"]
+ip = "im.todus.cu"
+port = 22
 #ip = ['google.com', 'yandex.ru', 'facebook.com']
+retry = 1
+delay = 1
+timeout = 2
 
-for val in ip:
-    response = os.system('ping -c 1 ' + val)
-    if response == 0:
-        print(val + ' is up!')
-        bot.send_message(channel, val + ' is up')
-    else:
-        print(val + ' is down!')
-        bot.send_message(channel, val + ' is down!')
+def isOpen(ip, port):
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+s.connect((ip, int(port)))
+s.shutdown(2)
+return True
+except:
+return False
+
+def checkHost(ip, port):
+    ipup = False
+        for i in range(retry):
+    if isOpen(ip, port):
+        ipup = True
+    break
+        else:
+    time.sleep(delay)
+return ipup
+
+if checkHost(ip, port):
+print ip + " port " + str(port) + u" is \u001b[32;1mUP!\u001b[0m"
+bot.send_message(channel, val + ' is up')
+
+else:
+print ip + " port " + str(port) + u" is \u001b[31;1mDOWN!\u001b[0m"
+bot.send_message(channel, val + ' is down!')
