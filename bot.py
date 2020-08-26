8a177c8017ef71ba023fb469b65d8d21657ecd5e
raymand211092 <50679989+raymand211092@users.noreply.github.com>
2020-08-26 10:43:37 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ import telebot
 channel = '@teste_forw1'
 token = '903986387:AAEOTXBHwGkilq9PCvC9NkfohZH259H7pC0'
 bot = telebot.TeleBot(token)
-ip = ["www.todus.cu"]
-port = 22
+ip = ["im.todus.cu"]
+port = 8888
 #ip = ['google.com', 'yandex.ru', 'facebook.com']
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
