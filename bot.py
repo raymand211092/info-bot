d2fe34506f5ccf84fc3e457c47db8832009c1565
raymand211092 <50679989+raymand211092@users.noreply.github.com>
2020-08-26 10:00:10 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,17 @@
-import socket
-#import os
+import os
 import telebot
 
-channel = '-1001420899246'
+channel = '@teste_forw1'
 token = '903986387:AAEOTXBHwGkilq9PCvC9NkfohZH259H7pC0'
 bot = telebot.TeleBot(token)
-ip = "im.todus.cu"
-port = 22
+ip = ["10.5.110.55", "10.5.109.151", "10.5.110.56"]
 #ip = ['google.com', 'yandex.ru', 'facebook.com']
 
-def checkport(port):
-    socket_objet = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port_is = socket_objet.connect_ex((ip, port))
-    socket_objet.close()
-    if port_is == 0:
-        return ("toDus esta funcionando")
-        bot.send_message(channel, "toDus esta funcionando")
+for val in ip:
+    response = os.system('ping -c 1 ' + val)
+    if response == 0:
+        print(val + ' is up!')
+        bot.send_message(channel, val + ' is up')
     else:
-        return ("Hay problemas de conexión al toDus")
-        bot.send_message(channel, "Hay problemas de conexión al toDus")
-    
+        print(val + ' is down!')
+        bot.send_message(channel, val + ' is down!')
