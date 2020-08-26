00d76956b1b30576e70c13e778414da45038b49a
raymand211092 <50679989+raymand211092@users.noreply.github.com>
2020-08-26 10:47:09 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ channel = '@teste_forw1'
 token = '903986387:AAEOTXBHwGkilq9PCvC9NkfohZH259H7pC0'
 bot = telebot.TeleBot(token)
 ip = ["im.todus.cu"]
-port = 8888
+port = 22
 #ip = ['google.com', 'yandex.ru', 'facebook.com']
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
@@ -20,5 +20,7 @@ def port_scan(port):
   
 if port_scan(port): 
     print('Port', port, 'is open') 
+    bot.send_message(channel, port + ' is up')
 else: 
     print("port", port, "is closed")
+    bot.send_message(channel, port + ' is down')
